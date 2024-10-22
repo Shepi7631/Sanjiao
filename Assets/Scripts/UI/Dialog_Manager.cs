ce90f96a66be3a34b0514f77ecfdc4512681858c
wm02 <12706819+wm02@user.noreply.gitee.com>
2024-10-23 03:02:54 +0800

    完成对话功能的大部分
    完成按键推进对话，对话分支
    但是对话开始如何触发，
    以及对话结束要如何去除UI
    对话内容改变剧情走向要怎么搞
    还不太清楚

--- Assets/Scripts/UI/Dialog_Manager.cs
+++ Assets/Scripts/UI/Dialog_Manager.cs
@@ -2,53 +2,115 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Dialog_Manager : MonoBehaviour
 {
-    //对话文本文件，csv格式
-    public TextAsset dialogDataFile;
+   
+    public TextAsset dialogDataFile;  //对话文本文件，csv格式   
+    public SpriteRenderer figure;//角色图像
+    public TMP_Text nameText;//角色名字文本
+    public TMP_Text dialogText; //对话内容文本
+    public List<Sprite> sprites = new List<Sprite>();//角色图片列表
+    public Button nextbutton;//continue按钮
+    public GameObject optionButton;//选项按钮
+    public Transform buttonGroup;//选项按钮的排列方式
+    Dictionary<string,Sprite> imageDic=new Dictionary<string, Sprite>();//角色名字对应立绘字典
+    public int dialogIndex;//保存当前对话索引值
 
-    //角色图像
-    public SpriteRenderer figure;
+    //对话文本按行分割
+    public string[] dialogRows;
 
-    //角色名字文本
-    public TMP_Text nameText;
+    public void OnClickNext()
+    {
+        ShowDialogRow();
+    }//按下按钮后，会显示下一个文本
 
-    //对话内容文本
-    public TMP_Text dialogText;
+    private void Awake()
+    {
+        imageDic["Player_Idle_0"] = sprites[0];
+        imageDic["Player_Idle_2"] = sprites[1];
+    }//将图片素材存入数组
 
-    //角色图片列表
-    public List<Sprite> sprites = new List<Sprite>();
-
-    //角色名字对应立绘字典
-    Dictionary<string,Sprite> imageDic=new Dictionary<string, Sprite>();
     public void UpdateText(string _name, string _text)
     {
         nameText.text = _name;
         dialogText.text = _text;
 
-    }
+    }//更新文本
 
     public void UpdateImage(string _name)
     {
         figure.sprite = imageDic[_name];
 
-    }
+    }//更新图像
 
-    public void ReadText()
+    public void ReadText(TextAsset _textAsset)
     {
+        dialogRows = _textAsset.text.Split('\n');
+  
+         Debug.Log("load success");
+    }//读取对话文件
+
+    public void ShowDialogRow()//展示对话文件对应的对话与立绘
+    {
+        for (int i=0;i<dialogRows.Length;i++)
+        {
+            string[] cells = dialogRows[i].Split(',');
+            if (cells[0]=="#"&& int.Parse(cells[1]) == dialogIndex)
+            {
+                UpdateText(cells[2], cells[3]);//cells[2]是人名，cells[3]是对话内容
+                UpdateImage(cells[2]);
+                dialogIndex =int.Parse( cells[4]);//cells[4]是跳转序号
+                break;
+            }
+            else if (cells[0]=="&"&& int.Parse(cells[1]) == dialogIndex)
+            {
+                nextbutton.gameObject.SetActive(false);
+                GenerateOption(i);
+            }
+            else if (cells[0] == "END" && int.Parse(cells[1]) == dialogIndex)
+            {
+                nextbutton.gameObject.SetActive(false);
+            }
+        }
 
     }
+
+    public void GenerateOption(int _index)
+    {
+        string[] cells = dialogRows[_index].Split(',');
+        if (cells[0] == "&") 
+        {
+            GameObject button = Instantiate(optionButton, buttonGroup);//绑定按钮实例
+
+            button.GetComponentInChildren<TMP_Text>().text = cells[3];//cells[3]是对话内容
+            button.GetComponent<Button>().onClick.AddListener(
+                delegate 
+                { 
+                OnOptionClick(int.Parse(cells[4])); //cells[4]是需要跳转的序号
+                }
+            );
+            GenerateOption(_index + 1);
+        }
     
-    private void Awake()
+     }//产生选项按钮
+
+    public void OnOptionClick(int _id)
     {
-        imageDic["Player_Idle_0"] = sprites[0];
-        imageDic["Player_Idle_2"] = sprites[1];
-    }//将图片存入数组
+        dialogIndex = _id;
+        ShowDialogRow();
+        for(int i=0;i<buttonGroup.childCount;i++)
+        {
+            Destroy(buttonGroup.GetChild(i).gameObject);
+         }
+        nextbutton.gameObject.SetActive(true);
+    }//选项按键事件
+    
     private void Start()
     {
-        UpdateText("主角","这是测试文本");
-        UpdateImage("Player_Idle_2");
+        ReadText(dialogDataFile);
+        ShowDialogRow();
     }
 
 }
